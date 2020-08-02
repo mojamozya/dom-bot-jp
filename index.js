f7216f2053a39407d34b025c5b1205ce691c7742
Mojamozya <s.fujitani@uzuz.jp>
2020-08-03 01:54:04 +0900

    Initial

---
+++ index.js
@@ -0,0 +1,29 @@
+//ログイン処理
+require('dotenv').config();
+const Discord = require('discord.js');
+const client = new Discord.Client();
+client.on('ready', () => {
+    console.log('ready...');
+});
+
+client.on('message', async message => {
+    if (message.content.startsWith('Randomly-generated Kingdom`\n')) {
+        const secondRow = message.content.split('\n')[1];
+        const cardsString = secondRow.slice(0, secondRow.length - 1);
+        const cards = cardsString.split(', ');
+        for (const c of cards) {
+            const cardMessage = await message.channel.send('!text ' + c);
+            cardMessage.delete();
+        }
+
+        sleep(10 * 1000, function () {
+            message.channel.send('!kingdom -k ' + cardsString);
+            message.delete();
+        })
+    }
+});
+client.login(process.env.BOT_TOKEN);
+
+function sleep(waitSec, callback) {
+    setTimeout(callback, waitSec);
+}
